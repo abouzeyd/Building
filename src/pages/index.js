14eb5b797a28d3265df3311d24efc39285f337a6
abouzeyd <mohoumouhamed01@gmail.com>
2020-12-25 13:02:06 +0000

    feats:tester

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
 
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
