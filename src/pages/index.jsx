be836aa79a7a1fd893c40ddc426e6252ce474977
abouzeyd <mohoumouhamed01@gmail.com>
2020-12-27 22:46:23 +0000

    feats:index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -127,7 +127,7 @@ const IndexPage = () => {
           }}
         >
           <div sx={{ textAlign: ["center", "left"], ml: [0, 95] }}>
-            <h2 sx={{ width: [300, 300], color: "orange" }}>{text}</h2>
+            <h2 sx={{ width: [350, 300], color: "orange" }}>{text}</h2>
             <div sx={{ textAlign: ["left"] }}>
               <p sx={{ width: [300, 300], mx: "auto" }}>{paragraph}</p>
             </div>
