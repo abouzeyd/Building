d6524b4c6513bcb0cb67e5ac8b09556264a91f89
abouzeyd <mohoumouhamed01@gmail.com>
2021-01-16 19:20:09 +0000

    feats:nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -58,8 +58,8 @@ const Nav = () => {
         sx={{
           display: "flex",
           alignItems: "flex-end",
-          // flex: 1,
-          mr: [0, 0, 0, 20],
+          flex: 1,
+          ml: [0, 0, -50, 70],
         }}
       >
         <List>
